7e51d1347e236cf04f14b3d1f50c48b7c10fc309
Steve Newns <s.newns92@gmail.com>
2023-05-06 22:00:04 +0000

    add a variable to green taxi data staging to decide if this is a test run (limit to 100 rows) or not

--- week4_analytics_engineering/dbt_cloud/models/staging/stg_green_trip_data.sql
+++ week4_analytics_engineering/dbt_cloud/models/staging/stg_green_trip_data.sql
@@ -33,4 +33,11 @@ select
     {{ get_payment_type_description('payment_type') }} as payment_type_description,  {# macro #}
     cast(congestion_surcharge as numeric) as congestion_surcharge
 from {{ source('staging', 'green_trip_data') }}
-limit 100
+where vendorid is not null
+
+-- dbt build -m [model.sql] --var 'is_test_run: false'
+{% if var('is_test_run', default=true) %}
+    
+    limit 100
+
+{% endif %}
