84c3b608fb75fe0e3617f816c1c652ea5f171b53
unknown <s.newns92@gmail.com>
2024-01-03 20:37:23 -0500

    finish Docker video for week1 for 2024 cohort of DE Zoomcamp

---
+++ week1_intro_prereqs_environment/pipeline.py
@@ -0,0 +1,13 @@
+import sys
+import pandas as pd
+
+# print out all the args
+print(sys.argv) # arg[0] = name of file, arg[1] = whatever we pass
+
+# store what we pass in as an arg into a vriable
+day = sys.argv[1]
+
+# TODO
+# Fancy data manipulation
+
+print(f"Job successfully finished for day = {day}")
