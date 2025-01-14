4abed9958d8c17fa516ab75234791a4949cb96a1
unknown <s.newns92@gmail.com>
2025-01-13 20:22:52 -0500

    clean up for 2025 course

--- project/metabase_queries/most_popular_genres.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-	public.dim_genres.genre_name,
-	COUNT(public.dim_genres.genre_name) AS genre_count
-FROM public.dim_genres_bridge
-LEFT JOIN public.dim_genres ON
-	public.dim_genres_bridge.genre_id = public.dim_genres.genre_id
-GROUP BY public.dim_genres.genre_name
-ORDER BY genre_count DESC
-;
