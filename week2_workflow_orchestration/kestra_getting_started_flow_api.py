e771dfb20a512893bc056976ec0c71e9725d2bfe
unknown <s.newns92@gmail.com>
2025-01-19 12:36:49 -0500

    Send Discord message through a Discord webhook task in Kestra

--- week2_workflow_orchestration/kestra_getting_started_flow_api.py
+++ week2_workflow_orchestration/kestra_getting_started_flow_api.py
@@ -1,7 +1,11 @@
 import requests
+from kestra import Kestra
 
 r = requests.get('https://api.github.com/repos/kestra-io/kestra')
 
 gh_stars = r.json()['stargazers_count']
 
-print(gh_stars)
+# print(gh_stars)
+
+# Make the output available in the Kestra platform
+Kestra.outputs({'gh_stars': gh_stars})
