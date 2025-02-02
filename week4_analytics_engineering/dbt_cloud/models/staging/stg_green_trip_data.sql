5233ef90ed7c505d65d9b34600f5e193df9d8c62
Steve Newns <s.newns92@gmail.com>
2025-02-02 18:21:58 +0000

    Create row numbers to find duplicate trips

--- week4_analytics_engineering/dbt_cloud/models/staging/stg_green_trip_data.sql
+++ week4_analytics_engineering/dbt_cloud/models/staging/stg_green_trip_data.sql
@@ -1,10 +1,18 @@
 -- Create a view so we don't have the need to refresh constantly but still have the latest data loaded
-{{ config(materialized = 'view') }}
+{{
+    config(
+        materialized = 'view'
+    ) 
+}}
 
-WITH source AS (
+-- Generate row numbers to find duplicates records (vendor_id and lpep_pickup_datetime)
+WITH trip_data AS (
     /* {# SELECT * FROM {{ source(<'source-name-from-schema.yml>', '<table-name-from-schema.yml>') }} #} */
-    SELECT * FROM {{ source('staging', 'green_trip_data') }}
-    LIMIT 100
+    SELECT
+        ROW_NUMBER() OVER(PARTITION BY vendor_id, lpep_dropoff_datetime) AS row_num,
+    FROM {{ source('staging', 'green_trip_data') }}
+    -- Remove null data
+    WHERE vendor_id IS NOT NULL
 )
 ,
 
@@ -32,7 +40,9 @@ renamed AS (
         {{ get_payment_type_description('payment_type') }} AS payment_type_description,  {# custom macro #}
         trip_type,
         congestion_surcharge
-    FROM source
+    FROM trip_data
+    -- Retrieve only one of any duplicate records
+    WHERE row_num = 1
 )
 
 SELECT
